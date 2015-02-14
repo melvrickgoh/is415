58f43fec0198e7f0cde0318829739328b9ee1ee6
Melvrick <melvrickgoh@hotmail.com>
2015-02-14 18:58:56 +0800

    soon soon

--- index.js
+++ index.js
@@ -87,4 +87,5 @@ app.get('/api/iron/get',function(req,res){
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
+  console.log('hello');
 });
